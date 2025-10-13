6aa85eba87cd8a6b509c34fba609afd6adde4357
Peru Dayani <perudayani@gmail.com>
2025-10-13 02:42:59 -0400

    all right

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -2,6 +2,9 @@ import { getProfileAction } from "@/app/actions/profileActions";
 import DisplayError from "@/app/components/DisplayError/DisplayError";
 import ProfileEditor from "@/app/components/ProfileEditor/ProfileEditor";
 
+// Tell Next.js that this page depends on cookies
+export const dynamic = "force-dynamic";
+
 export default async function ProfilePage() {
   const profileResponse = await getProfileAction();
 
