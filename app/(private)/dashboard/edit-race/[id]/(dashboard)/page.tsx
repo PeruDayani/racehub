3ebfc256fcaa25e87779e25b3896573f71ef2769
Colin O'Neill <colinfoneill@gmail.com>
2025-11-20 08:19:50 -0600

    adding dashboard stats for signups

--- app/(private)/dashboard/edit-race/[id]/(dashboard)/page.tsx
+++ app/(private)/dashboard/edit-race/[id]/(dashboard)/page.tsx
@@ -1,3 +1,15 @@
-export default async function EditRaceDashboard() {
-  return <div>Edit Race Dashboard</div>;
+import DashboardEditor from "@/app/components/RaceEditor/Dashboard/DashboardEditor";
+
+type DashboardPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function EditRaceDashboard({
+  params,
+}: DashboardPageProps) {
+  const { id } = await params;
+  const raceId = Number.parseInt(id, 10);
+  return <DashboardEditor raceId = {raceId} />;
 }
---
+++ app/actions/dashboardActions.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import { count, eq, sum } from "drizzle-orm";
+import { tickets } from "../../drizzle/schema";
+import { db } from "../lib/db";
+
+type GetRaceSignupStatsResponse = {
+    success: boolean;
+    message: string;
+    data?: {
+        registrantCount: number;
+    };
+};
+
+export async function getRaceSignupStatsAction (
+    raceId: number,
+): Promise<GetRaceSignupStatsResponse> {
+    try {
+        if (!raceId || Number.isNaN(raceId)) {
+          return {
+            success: false,
+            message: "A valid race id is required to fetch stats.",
+          };
+        }
+
+    const [result] = await db
+        .select({
+            registrantCount: count(),
+        })
+        .from(tickets)
+        .where(eq(tickets.raceId, raceId));
+
+    return {
+        success: true,
+        message: "Race signup stats retrieved.",
+        data: {
+            registrantCount: result?.registrantCount ?? 0
+        }
+    }
+
+    }
+    catch (error) {
+        console.error("Error getting race signup stats:", error);
+        return {
+          success: false,
+          message:
+            error instanceof Error
+              ? error.message
+              : "Failed to get race signup stats.",
+        };
+      }
+}
